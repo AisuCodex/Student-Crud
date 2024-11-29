366243bb5cccc15c155a6dcc36e223cc78f5465c
AisuCodex <marcashleycrisostomo53@gmail.com>
2024-11-29 12:55:52 +0800

    asd

--- edit.php
+++ edit.php
@@ -30,31 +30,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>Edit Student</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h1>Edit Student</h1>
-<form method="POST">
-    <label>ID Number:</label>
-    <input type="text" name="id_number" value="<?= $student['id_number'] ?>" required><br>
-    <label>Name:</label>
-    <input type="text" name="name" value="<?= $student['name'] ?>" required><br>
-    <label>Birthday:</label>
-    <input type="date" name="birthday" value="<?= $student['birthday'] ?>" required><br>
-    <label>Age:</label>
-    <input type="number" name="age" value="<?= $student['age'] ?>" required><br>
-    <label>Gender:</label>
-    <select name="gender" required>
-        <option value="Male" <?= $student['gender'] == 'Male' ? 'selected' : '' ?>>Male</option>
-        <option value="Female" <?= $student['gender'] == 'Female' ? 'selected' : '' ?>>Female</option>
-        <option value="Other" <?= $student['gender'] == 'Other' ? 'selected' : '' ?>>Other</option>
-    </select><br>
-    <label>Blood Type:</label>
-    <input type="text" name="bloodtype" value="<?= $student['bloodtype'] ?>"><br>
-    <label>Year Level:</label>
-    <input type="text" name="year_level" value="<?= $student['year_level'] ?>"><br>
-    <label>Section:</label>
-    <input type="text" name="section" value="<?= $student['section'] ?>"><br>
-    <button type="submit">Update</button>
-</form>
+<div id="addStudentForm" style="display: block;">
+    <form method="POST">
+        <div class="form-group">
+            <label>ID Number:</label>
+            <input type="text" name="id_number" value="<?= $student['id_number'] ?>" required><br>
+        </div>
+        <div class="form-group">
+            <label>Name:</label>
+            <input type="text" name="name" value="<?= $student['name'] ?>" required><br>
+        </div>
+        <div class="form-group">
+            <label>Birthday:</label>
+            <input type="date" name="birthday" value="<?= $student['birthday'] ?>" required><br>
+        </div>
+        <div class="form-group">
+            <label>Age:</label>
+            <input type="number" name="age" value="<?= $student['age'] ?>" required><br>
+        </div>
+        <div class="form-group">
+            <label>Gender:</label>
+            <select name="gender" required>
+                <option value="Male" <?= $student['gender'] == 'Male' ? 'selected' : '' ?>>Male</option>
+                <option value="Female" <?= $student['gender'] == 'Female' ? 'selected' : '' ?>>Female</option>
+                <option value="Other" <?= $student['gender'] == 'Other' ? 'selected' : '' ?>>Other</option>
+            </select><br>
+        </div>
+        <div class="form-group">
+            <label>Blood Type:</label>
+            <input type="text" name="bloodtype" value="<?= $student['bloodtype'] ?>"><br>
+        </div>
+        <div class="form-group">
+            <label>Year Level:</label>
+            <input type="text" name="year_level" value="<?= $student['year_level'] ?>"><br>
+        </div>
+        <div class="form-group">
+            <label>Section:</label>
+            <input type="text" name="section" value="<?= $student['section'] ?>"><br>
+        </div>
+        <button type="submit" class="toggle-btn">Update Student</button>
+        <a href="index.php" class="toggle-btn" style="text-decoration: none; display: inline-block; background-color: #808080;">Back</a>
+    </form>
+</div>
 </body>
 </html>
